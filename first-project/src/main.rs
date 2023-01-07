c5fbbf68add7f250abc05d716d09dc84963e258b
KazumasaYasui <silver.soul39@gmail.com>
2023-01-07 21:42:41 +0900

    タプル型、条件分岐

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     variables();
+    conditional_branch();
 }
 
 fn variables() {
@@ -26,7 +27,26 @@ fn variables() {
     vec[0] = 10;
     vec.push(10);
 
+    let t: (i32, &str) = (120, "文字列");
+    let t0 = t.0;
+    let t1 = t.1;
+
     println!("{}", final_string);
     println!("array's size is {}. array's last value is {}", array.len(), array[array.len() -1]);
     println!("vec's size is {}. vec's last value is {}", vec.len(), vec[vec.len() -1]);
+    println!("tuple t's elements are {} and {}", t0, t1);
+}
+
+fn conditional_branch() {
+    let x = 100;
+    let y = 50;
+
+    if x == y {
+        println!("x and y are same value!")
+    } else {
+        println!("x and y are not same value!")
+    }
+
+    let z = if x != y { 500 } else { 300 };
+    println!("z value is {}", z);
 }
